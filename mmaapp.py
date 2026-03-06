8fcc4d8daa5f23ed318e77e0f2eb3eee49aec53a
jennyheo <hyj3589@gmail.com>
2026-03-06 09:29:20 +0900

    Update mmaapp.py

--- mmaapp.py
+++ mmaapp.py
@@ -383,4 +383,4 @@ with tab3:
 
 st.divider()
 st.markdown("""<div style="text-align: right;"><a href="#top" style="text-decoration-line:none;font-size:25pt;"> 🔝</a></div>""", unsafe_allow_html=True)
-st.markdown('**강원지방병무청** (_Updated on 2025. 4. 21._)')
+st.markdown('**강원지방병무청** (_Updated on 2026. 3. 6._)')
